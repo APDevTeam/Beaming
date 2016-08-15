27495af39185f8eff4814baceb569c74753284c5
cccm5 <ccccm5@gmail.com>
2016-08-14 20:49:06 -0400

    change the way that the death message is displayed
    "player" died is no longer said in chat
    also changes encoding?

--- src/net/TylerS1066/Beaming/Main.java
+++ src/net/TylerS1066/Beaming/Main.java
@@ -6,44 +6,59 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 
-public class Main extends JavaPlugin {
-	
-	@Override
-	public void onEnable() {
+public class Main extends JavaPlugin implements Listener{
 
-	}
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
-	@Override
-	public void onDisable() {
+    @Override
+    public void onDisable() {
 
-	}
+    }
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("beam")) {
-			if (!(sender instanceof Player)) {
-				sender.sendMessage("Only players are allowed to use this command");
-				return false;
-			} else {
-				Player player = (Player) sender;
-				if (args.length == 1) {
-					if (args[0].equalsIgnoreCase("scotty"))
-						this.getServer().broadcastMessage(player.getDisplayName() + ": 'Beam me up scotty!'");
-				} else {
-					sender.sendMessage("§1[§eBeaming§1] §cYou beamed to you ship!");
-					String message = "§1[§eBeaming§1]§c" + player.getDisplayName() + "§r§c beamed to their ship";
-					Bukkit.broadcastMessage(message);
-				}
-				player.getWorld().setGameRuleValue("showDeathMessages" , "false");
-				player.setHealth(0);
-				player.spigot().respawn();
-				String message = "§1[§eBeaming§1]§c" + player.getDisplayName() + "§r§c beamed to their ship";
-				Bukkit.broadcastMessage(message);
-				player.getWorld().setGameRuleValue("showDeathMessages" , "true");		}
-			return true;
-		}
-		return false;
-	}
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("beam")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Only players are allowed to use this command");
+                return false;
+            } else {
+                Player player = (Player) sender;
+                player.setMetadata("beaming", new FixedMetadataValue(this,true));
+                if (args.length == 1) {
+                    if (args[0].equalsIgnoreCase("scotty"))
+                        this.getServer().broadcastMessage(player.getDisplayName() + ": 'Beam me up scotty!'");
+                } else {
+                    sender.sendMessage("Â§1[Â§eBeamingÂ§1] Â§cYou beamed to you ship!");
+                    String message = "Â§1[Â§eBeamingÂ§1]Â§c" + player.getDisplayName() + "Â§rÂ§c beamed to their ship";
+                    Bukkit.broadcastMessage(message);
+                }
+                //player.getWorld().setGameRuleValue("showDeathMessages" , "false");
+                player.setHealth(0);
+                player.spigot().respawn();
+                //String message = "Â§1[Â§eBeamingÂ§1]Â§c" + player.getDisplayName() + "Â§rÂ§c beamed to their ship";
+                //Bukkit.broadcastMessage(message);
+                //player.getWorld().setGameRuleValue("showDeathMessages" , "true");		
+            }
+            return true;
+        }
+        return false;
+    }
 
-
+    @EventHandler
+    public void onEntityDeath(PlayerDeathEvent e){
+        if(e.getEntity() instanceof Player){
+            Player p = (Player) e.getEntity();
+            if(p.hasMetadata("beaming") && ((FixedMetadataValue) p.getMetadata("beaming")).asBoolean() ){
+                e.setDeathMessage("Â§1[Â§eBeamingÂ§1]Â§c" + p.getDisplayName() + "Â§rÂ§c beamed to their ship");
+                p.setMetadata("beaming", new FixedMetadataValue(this,false));
+            }
+        }
+    }
 }
